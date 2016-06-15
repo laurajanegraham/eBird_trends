2a210acf958bc444626f5501454c308015991bb2
laurajanegraham <laurajaneegraham@gmail.com>
2016-06-15 06:24:33 -0400

    start script for pulling data in from SQL database and running occupancy model

---
+++ code/eBird_occmod.R
@@ -0,0 +1,12 @@
+# eBird_occmod.R: script to pull in data from SQL database and run multi-species occupancy model for each family ----
+
+# 1. Load packages ----
+library(taxize)
+library(RPostgreSQL)
+
+# 2. Get species list and append family ----
+con
+# 3. Get data from SQL in correct format ----
+# create SQL query
+# run 
+# 4. Set up for multi-species occupancy model ----
