5e16cab341a6066f055e4b5d831ec15a8e27f23f
laurajanegraham <laurajaneegraham@gmail.com>
2016-06-22 06:26:24 -0400

    move SQL extract out of R and into Python

--- code/eBird_occmod.R
+++ code/eBird_occmod.R
@@ -1,7 +1,5 @@
 # eBird_occmod.R: script to pull in data from SQL database and run multi-species occupancy model for each family ----
+# 1. Load packages ----
+library(RPostgreSQL)
 
-# 2. Get data from SQL in correct format ----
-# create SQL query
-SELECT 
-# run 
-# 3. Set up for multi-species occupancy model ----
+# 2. Set up for multi-species occupancy model ----
