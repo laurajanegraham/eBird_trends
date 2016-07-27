56ed48aa9a47a680cd6b01aab17cb7c8de069765
lorra <lgxlg3@nottingham.ac.uk>
2016-07-27 15:21:44 +0100

    update to correct small errors

--- code/dynocc.JAGS.R
+++ code/dynocc.JAGS.R
@@ -16,7 +16,7 @@ model {
   # Ecological submodel: Define state conditional on parameters
   for (j in 1:nsite){
     for (k in 1:nspecies){
-      z[1,j,k] ~ dbern(psi1)
+      z[1,j,k] ~ dbern(psi1[k])
       for (i in 2:nyear){
         muZ[i,j,k]<- z[i-1,j,k]*phi[i-1,k] + (1-z[i-1,j,k])*gamma[i-1,k]
         z[i,j,k] ~ dbern(muZ[i,j,k])
@@ -37,15 +37,15 @@ model {
   } #i
   
   # Derived parameters: Sample and population occupancy, growth rate and turnover
-  for (k in 1:nspecies){
-    psi[1,k] <- psi1
-    n.occ[1,k]<-sum(z[1,1:nsite,k])
-    for (i in 2:nyear){
-      psi[i,k] <- psi[i-1,k]*phi[i-1,k] + (1-psi[i-1,k])*gamma[i-1,k]
-      n.occ[i,k] <- sum(z[i,1:nsite,k])
-      growthr[i-1,k] <- psi[i,k]/psi[i-1,k]                         # originally we had growthr[k]. JAGS seem to dislike vectoring going from 2..K.
-      turnover[i-1,k] <- (1 - psi[i-1,k]) * gamma[i-1,k]/psi[i,k]
-    }
-  }
+  # for (k in 1:nspecies){
+  #   psi[1,k] <- psi1
+  #   n.occ[1,k]<-sum(z[1,1:nsite,k])
+  #   for (i in 2:nyear){
+  #     psi[i,k] <- psi[i-1,k]*phi[i-1,k] + (1-psi[i-1,k])*gamma[i-1,k]
+  #     n.occ[i,k] <- sum(z[i,1:nsite,k])
+  #     growthr[i-1,k] <- psi[i,k]/psi[i-1,k]                         # originally we had growthr[k]. JAGS seem to dislike vectoring going from 2..K.
+  #     turnover[i-1,k] <- (1 - psi[i-1,k]) * gamma[i-1,k]/psi[i,k]
+  #   }
+  # }
 }
 
