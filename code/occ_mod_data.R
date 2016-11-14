f31a5637c1573af107a8e4bfbf7a94806bd1c3c9
laurajanegraham <laurajaneegraham@gmail.com>
2016-11-14 11:31:15 +0000

    start of code for occupancy model

---
+++ code/occ_mod_data.R
@@ -0,0 +1,15 @@
+# occ_mod_data.R ---- 
+# Code to pull together the data for the occupancy model 
+
+# 1. eBird observation and obs covariate data ----
+files <- list.files("")
+# 2. Bird functional group data ----
+
+# 3. Environmental covariate data ----
+
+# 4. Bundle together data ----
+
+# 5. Run occupancy model ----
+
+
+
